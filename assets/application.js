f827c20c80b1e192fa4a6aa4605bff878acdf915
Jack Wuersch <jackwuersch@gmail.com>
2020-06-23 14:50:23 +0900

    Add to cart is now async JavaScript. No longer redirects to the cart.

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,32 @@
 // Put your applicaiton javascript here
+
+/*
+Ajax Add to cart function, vanilla Javascript
+*/
+async function addToCart() {
+  //Get quantity and variantId of product
+  const variantId = document.getElementById("productSelect").value;
+  const quantity = document.getElementById("Quantity").value;
+
+  //Fetch request
+  const result = await fetch("/cart/add.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "X-Requested-With": XMLHttpRequest,
+    },
+    body: JSON.stringify({
+      id: variantId,
+      quantity: quantity,
+    }),
+  });
+
+  const data = await result.json();
+  console.log(data);
+
+  // Let customer know if there was an error. Can't add anymore etc.
+  if (data.status === 422) {
+    alert(data.description);
+  }
+}
